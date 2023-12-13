186c788b453bd082b979fb414b2c25d30c0db076
EL JOHRI Hamza <Hamza.ELJOHRI@accor.com>
2023-12-13 08:40:53 +0100

    change ec2 ami

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 #create the EC2 ressource
 resource "aws_instance" "ec2_instance" {
   for_each               = local.ec2_instances
-  ami                    = data.aws_ami.ec2_ami[each.key].image_id
+  ami                    = each.value.system.ami_id
   instance_type          = each.value.system.instance_type
   subnet_id              = each.value.network.subnet_id
   vpc_security_group_ids = [aws_security_group.ec2_sg[each.value.network.security_group].id]
